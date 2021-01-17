74bdc23bcd28e19a67f2e0d7950bcd2937ea13b5
Sander <sander_nijhuis@live.nl>
2021-01-17 13:13:42 +0100

    add(main): Adds ability to read env variables from .env.local

--- src/services/UserDataService.js
+++ src/services/UserDataService.js
@@ -1,42 +1,35 @@
 import axios from 'axios'
-const USER_API_URL = 'http://localhost:8080/user'
 
 class UserDataService {
 
     // eslint-disable-next-line
-    retrieveAllUsers(name) {
-        //console.log('executed service')
-        return axios.get(`${USER_API_URL}/list`);
-    }
-
-    // eslint-disable-next-line
     retrieveUser(id) {
         //console.log('executed service')
-        return axios.get(`${USER_API_URL}/${id}`);
+        return axios.get(`${process.env.VUE_APP_USER_API_URL}/${id}`);
     }
 
     // eslint-disable-next-line
     deleteUser(name, id) {
         //console.log('executed service')
-        return axios.delete(`${USER_API_URL}/${id}`);
+        return axios.delete(`${process.env.VUE_APP_USER_API_URL}/user/${id}`);
     }
 
     // eslint-disable-next-line
     updateUser(name, id, user) {
         //console.log('executed service')
-        return axios.put(`${USER_API_URL}/${id}`, user);
+        return axios.put(`${process.env.VUE_APP_USER_API_URL}/user/${id}`, user);
     }
 
     // eslint-disable-next-line
     login(name, user) {
         //console.log('executed service')
-        return axios.post(`${USER_API_URL}/login`, user);
+        return axios.post(`${process.env.VUE_APP_USER_API_URL}/login`, user);
     }
 
     // eslint-disable-next-line
     createUser(name, user) {
         //console.log('executed service')
-        return axios.post(`${USER_API_URL}/create`, user);
+        return axios.post(`${process.env.VUE_APP_USER_API_URL}/create`, user);
     }
 }
 
